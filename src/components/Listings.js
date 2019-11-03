ed878dda8c0c19b656cee78b38d7940924f2ee7d
Sean Philippi <sean.philippi@gmail.com>
2019-11-02 22:24:14 -0500

    mapping json data

--- src/components/Listings.js
+++ src/components/Listings.js
@@ -1,38 +1,40 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-
 import { TableRow, Table, TableHead, TableCell, TableBody } from '@material-ui/core';
+const data = require('../listings.json');
+
 
 const useStyles = makeStyles({
   table: {
-    minWidth: 650
+    maxWidth: '90vw',
+    margin: 'auto'
   }
 });
 
-const Listings = ({ listings }) => {
+const Listings = props => {
   const classes = useStyles();
   return (
     <Table className={ classes.table }>
       <TableHead>
         <TableRow>
-          <TableCell align="right">Name</TableCell>
-          <TableCell align="right">Description</TableCell>
-          <TableCell align="right">Hours</TableCell>
-          <TableCell align="right">Address</TableCell>
-          <TableCell align="right">Delete</TableCell>
+          <TableCell align="left">Name</TableCell>
+          <TableCell align="left">Description</TableCell>
+          <TableCell align="left">Hours</TableCell>
+          <TableCell align="left">Address</TableCell>
+          <TableCell align="left">Delete</TableCell>
         </TableRow>
       </TableHead>
-      <TableBody>
+      <TableBody className="">
         {
-          listings.map(listing => {
+          data.map(listing => {
             return (
-              <>
-                <TableCell align="right">{ listing.name }</TableCell>
-                <TableCell align="right">{ listing.description }</TableCell>
-                <TableCell align="right">{ listing.hours }</TableCell>
-                <TableCell align="right">{ listing.address }</TableCell>
-                <TableCell align="right">{ listing.delete }</TableCell>
-              </>
+              <TableRow>
+                <TableCell align="left">{ listing.name }</TableCell>
+                <TableCell align="left">{ listing.description }</TableCell>
+                <TableCell align="left">{ listing.hours }</TableCell>
+                <TableCell align="left">{ listing.address }</TableCell>
+                <TableCell align="left">{ listing.delete }</TableCell>
+              </TableRow>
             )
           })
         }
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import './index.css';
 import App from './App';
 import store from './redux/store';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <BrowserRouter>
